5c71e7f6127831d3040f1858900f3b8c543c5f96
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2017-05-19 22:50:08 -0500

    Add authentication via the API and save the authentication headers needed returned form the API call into a shared preference

--- app/src/main/java/mx/unam/passlist/Login.java
+++ app/src/main/java/mx/unam/passlist/Login.java
@@ -1,18 +1,32 @@
 package mx.unam.passlist;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.OkHttpResponseAndJSONObjectRequestListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Response;
 
 public class Login extends AppCompatActivity {
     public EditText etEmail;
     public EditText etPassword;
     public Button bntSend;
     public TextView tvRegister;
+    public SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +37,13 @@ public class Login extends AppCompatActivity {
         etPassword = (EditText) findViewById(R.id.et_password);
         bntSend = (Button) findViewById(R.id.btn_send);
         tvRegister = (TextView) findViewById(R.id.tv_register);
+        preferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
         //start btnSend click listener function
         bntSend.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
-
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                login(email, password);
             }
         });
 
@@ -37,5 +54,74 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+
+    private void login(String email, String password) {
+        if(email.equals("") || password.equals("")) {
+            Toast.makeText(this, "Por favor completa todos los campos", Toast.LENGTH_LONG).show();
+        } else {
+            attemptLogin("https://unam-passlist.herokuapp.com/auth/sign_in", email, password);
+        }
+    }
+
+    private void attemptLogin(String signInUrl, String email, String password) {
+        /** Create a JSON object like the following
+         {
+             "user": {
+                 "email": "example@email.com",
+                 "password": "password"
+             }
+         }
+         */
+        JSONObject jsonObject = new JSONObject();
+        JSONObject userCredentials = new JSONObject();
+        try {
+            userCredentials.put("email", email);
+            userCredentials.put("password", password);
+            jsonObject.put("user", userCredentials);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Send a HTTP post request to the sign_in API endpoint
+        AndroidNetworking.post(signInUrl)
+            .addJSONObjectBody(jsonObject)
+            .setTag("signin")
+            .setPriority(Priority.MEDIUM)
+            .build()
+            .getAsOkHttpResponseAndJSONObject(new OkHttpResponseAndJSONObjectRequestListener() {
+                @Override
+                public void onResponse(Response okHttpResponse, JSONObject response) {
+
+                    // handle success
+                    SharedPreferences.Editor editor = preferences.edit();
+                    // save the returned token into a shared preference
+                    editor.putString("Access-Token", okHttpResponse.header("Access-Token"));
+                    editor.putString("Client", okHttpResponse.header("Access-Token"));
+                    editor.putString("Expiry", okHttpResponse.header("Access-Token"));
+                    editor.putString("Uid", okHttpResponse.header("Access-Token"));
+                    editor.apply();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+//                    Log.i("HEADERS", okHttpResponse.headers().toString());
+//                    Log.i("Token", okHttpResponse.header("Access-Token"));
+//                    Log.i("Client", okHttpResponse.header("Client"));
+//                    Log.i("Expiry", okHttpResponse.header("Expiry"));
+//                    Log.i("Uid", okHttpResponse.header("Uid"));
+                }
+
+                @Override
+                public void onError(ANError error) {
+                    // handle error
+                    Toast.makeText(getApplicationContext(), "Failed login", Toast.LENGTH_LONG).show();
+                    if (error.getErrorCode() != 0) {
+                        // received error from server
+                        Log.e("ERROR", "onError errorBody : " + error.getErrorBody());
+                    } else {
+                        // error.getErrorDetail() : connectionError, parseError, requestCancelledError
+                        Log.e("FATAL", "onError errorDetail : " + error.getErrorDetail());
+                    }
+                }
+            });
+    }
 }
-.
--- app/src/main/java/mx/unam/passlist/MainActivity.java
+++ app/src/main/java/mx/unam/passlist/MainActivity.java
@@ -1,13 +1,30 @@
 package mx.unam.passlist;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences preferences;
+    private static final String AUTH_FIELD = "Access-Token";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        preferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (preferences.contains(AUTH_FIELD)) {
+
+        } else {
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+        }
     }
 }
