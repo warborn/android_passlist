19afcd0d5cbbbd3b5b5284b8cdf8e178ff179ea1
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2017-05-20 19:31:39 -0500

    Add and use methods in the PasslistService class to get all the groups of the current user, get the information of a sigle group and of a single class

--- app/src/main/java/mx/unam/passlist/MainActivity.java
+++ app/src/main/java/mx/unam/passlist/MainActivity.java
@@ -10,23 +10,33 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences preferences;
-    TextView tvMessage;
     ProgressBar pbLoadingIndicator;
+    // TODO: Remove if necessary
+    TextView tvMessage;
+    TextView tvGroups;
+    TextView tvGroup;
+    TextView tvClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         preferences = getSharedPreferences("CurrentUser", MODE_PRIVATE);
-        tvMessage = (TextView) findViewById(R.id.tv_message);
         pbLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+        // TODO: Remove if necessary
+        tvMessage = (TextView) findViewById(R.id.tv_message); // Hold the user name/email message
+        tvGroups = (TextView) findViewById(R.id.tv_groups);   // Hold a JSON string of all the groups of the logged in user
+        tvGroup = (TextView) findViewById(R.id.tv_group);     // Hold a JSON string of single group (including the classes calendar)
+        tvClass = (TextView) findViewById(R.id.tv_class);     // Hold a JSON string of a single class (including the list of students)
     }
 
     @Override
@@ -39,7 +49,10 @@ public class MainActivity extends AppCompatActivity {
                 pbLoadingIndicator.setVisibility(View.INVISIBLE);
                 try {
                     if (response.getBoolean("success")) {
-                        displayUserGroups(response.getJSONObject("data"));
+                        displayUserInfo(response.getJSONObject("data"));
+                        displayGroups();
+                        displayGroup();
+                        displayClass();
                     } else {
                         returnToLoginActivity();
                     }
@@ -57,8 +70,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // Currently this method displays the user name in a TextView
-    private void displayUserGroups(JSONObject data) {
+    // TODO: Remove if necessary
+    private void displayUserInfo(JSONObject data) {
         try {
             String message = "Hola " + data.getString("first_name") + "/" + data.getString("email");
             Log.i("USER_MESSAGE", message);
@@ -67,6 +80,62 @@ public class MainActivity extends AppCompatActivity {
         } catch(JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    // TODO: Use the JSONArray response to display the groups to the user
+    private void displayGroups() {
+        PasslistService.getGroups(new JSONArrayRequestListener() {
+            @Override
+            public void onResponse(JSONArray response) {
+                tvGroups.setText(response.toString());
+                tvGroups.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onError(ANError anError) {
+                anError.printStackTrace();
+            }
+        });
+    }
+
+    // TODO: Move to the activity where the user will see the classes calendar
+    // TODO: Use the JSONObject to display the group information (optionally)
+    // TODO: Use the JSONObject to display all of the classes in the calendar
+    private void displayGroup() {
+        String groupId = "1";
+        PasslistService.getGroup(groupId, new JSONObjectRequestListener() {
+            @Override
+            public void onResponse(JSONObject response) {
+                tvGroup.setText(response.toString());
+                tvGroup.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onError(ANError anError) {
+                anError.printStackTrace();
+                Log.e("GROUP_ERROR", anError.getErrorBody());
+            }
+        });
+    }
+
+    // TODO: Move to the activity where the user will see the table of students and pass list
+    // TODO: Use the JSONObject to display the class information (optionally)
+    // TODO: Use the JSONObject to display all of the students in the calendar
+    private void displayClass() {
+        String classId = "1";
+        PasslistService.getClass(classId, new JSONObjectRequestListener() {
+            @Override
+            public void onResponse(JSONObject response) {
+                tvClass.setText(response.toString());
+                tvClass.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onError(ANError anError) {
+                anError.printStackTrace();
+                Log.e("CLASS_ERROR", anError.getErrorBody());
+            }
+        });
     }
 
     private void returnToLoginActivity() {
--- app/src/main/java/mx/unam/passlist/PasslistService.java
+++ app/src/main/java/mx/unam/passlist/PasslistService.java
@@ -5,10 +5,14 @@ import android.content.SharedPreferences;
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.ANRequest;
 import com.androidnetworking.common.Priority;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.androidnetworking.interfaces.OkHttpResponseAndJSONObjectRequestListener;
 
 import org.json.JSONObject;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Ivan on 20/05/2017.
@@ -18,6 +22,9 @@ public final class PasslistService {
     private static final String API_URL = "https://unam-passlist.herokuapp.com";
     private static final String SIGNIN_URL = API_URL + "/auth/sign_in";
     private static final String VALIDATE_TOKEN_URL = API_URL + "/auth/validate_token";
+    private static final String GROUPS_URL = API_URL + "/groups";
+    private static final String GROUP_URL = API_URL + "/groups/{id}";
+    private static final String CLASS_URL = API_URL + "/classes/{id}";
 
     public static final void login(String email, String password, OkHttpResponseAndJSONObjectRequestListener requestListener) {
         JSONObject credentials = AuthUtils.createJsonCredentials(email, password);
@@ -39,5 +46,43 @@ public final class PasslistService {
                 .setPriority(Priority.MEDIUM)
                 .build()
                 .getAsJSONObject(requestListener);
+    }
+
+    public static final void getGroups(JSONArrayRequestListener requestListener) {
+        ANRequest.GetRequestBuilder androidNetworking = (ANRequest.GetRequestBuilder)
+                AuthUtils.addAuthHeaders(AndroidNetworking.get(GROUPS_URL));
+
+        androidNetworking.setTag("get_groups")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONArray(requestListener);
+    }
+
+    public static final void getGroup(String id, JSONObjectRequestListener requestListener) {
+        String groupURL = injectIdInUrl(GROUP_URL, id);
+        ANRequest.GetRequestBuilder androidNetworking = (ANRequest.GetRequestBuilder)
+                AuthUtils.addAuthHeaders(AndroidNetworking.get(groupURL));
+
+        androidNetworking.setTag("get_group")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(requestListener);
+    }
+
+    public static final void getClass(String id, JSONObjectRequestListener requestListener) {
+        String groupURL = injectIdInUrl(CLASS_URL, id);
+        ANRequest.GetRequestBuilder androidNetworking = (ANRequest.GetRequestBuilder)
+                AuthUtils.addAuthHeaders(AndroidNetworking.get(groupURL));
+
+        androidNetworking.setTag("get_class")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(requestListener);
+    }
+
+    private static final String injectIdInUrl(String urlPattern, String id) {
+        Pattern p = Pattern.compile("\\{id\\}", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(urlPattern);
+        return m.replaceAll(id);
     }
 }
